e494f4852d07fc8e43c033bd6f1047b32d48e88c
Selector <973663+Selectorrr@users.noreply.github.com>
2025-04-24 19:50:51 +0300

    release 0.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='svr_tts',
-    version='0.3',
+    version='0.4',
     packages=find_packages(),
     install_requires=[
         'onnxruntime-gpu',
