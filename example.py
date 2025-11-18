edf1678a2f7160d5c88abccc9781d13b3d43530a
selector <LitvinovSD@gmail.com>
2025-11-18 20:40:56 +0300

    поддержка исключений при расстановке ударений

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ from svr_tts.core import SynthesisInput
 
 if __name__ == '__main__':
     tts = SVR_TTS(api_key="selector:iX7bl2YQ")
-    wave, sr = soundfile.read('tmp/example.ogg')
+    wave, sr = soundfile.read('tmp/example.wav')
     wave_24k = resampy.resample(wave, sr, 24_000)
     outs = tts.synthesize_batch([
         SynthesisInput(text="Сбейте лестницу!", stress=True, timbre_wave_24k=wave_24k, prosody_wave_24k=wave_24k),
